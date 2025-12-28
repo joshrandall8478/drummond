c958b0cd37f08d847faf55c6992e69eb3274a181
Hunter Martin <175766562+huntermar3@users.noreply.github.com>
2025-12-28 15:39:43 -0500

    set up routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
+import { Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+
 function App() {
-  return <div>Hello World</div>
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
