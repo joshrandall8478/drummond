b90c4f5f52f49de72d4451d19e9f9e628b5bce71
Hunter Martin <175766562+huntermar3@users.noreply.github.com>
2025-12-28 16:58:17 -0500

    css for home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import "../css/Home.css"
 function Home() {
   return <>
     <div className="main-container">
       <div className="home-page-content">
-        <h1 className="title">DRUMMOND</h1>
-        <hr></hr>
+        
+        <div className="title-container">
+          <h1 className="title">DRUMMOND</h1>
+          <hr></hr>
+        </div>
 
         <div className = "game-options-container">
           <button className="game-option-button">STARTING 5 BATTLES</button>
