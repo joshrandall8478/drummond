c87d7437ad90f1c4cd987eb72d4f17e371456588
Hunter Martin <175766562+huntermar3@users.noreply.github.com>
2025-12-28 23:08:38 -0500

    html of login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,35 @@
 function Login() {
   return <>
     <div className="main-content">
-        <div>login</div>
+        <div className="login-page-container">
+
+            <div className="title-container">
+                <h1 className="title">DRUMMOND</h1>
+                <p className = "title-of-game">STARTING 5 BATTLES</p>
+            </div>
+
+            <div className = "login-container">
+                <div className="login-textview">LOGIN</div>
+
+                <div className = "username-container">
+                    <p className = "username-textview">Username</p>
+                    <input className="username"></input>
+                </div>
+
+                <div className = "password-container">
+                    <p className = "password-textview">Username</p>
+                    <input className="password"></input>
+                </div>
+
+                <button className="enter-game-button">ENTER GAME</button>
+
+                <button className="create-account-button">CREATE ACCOUNT</button>
+
+                <button className="back-to-menu-button">BACK TO MENU</button>
+
+
+            </div>
+        </div>
     </div>
   </>
 }
