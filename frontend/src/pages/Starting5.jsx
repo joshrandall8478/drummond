7c40810ed3dc0605b2f05f669bc006f8a94bbd83
Hunter Martin <175766562+huntermar3@users.noreply.github.com>
2026-01-08 02:03:49 -0500

    when game loads it loads all the players into state

--- frontend/src/pages/Starting5.jsx
+++ frontend/src/pages/Starting5.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from 'react';
 function Starting5(){
+    const [players, setPlayers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchPlayers = async () => {
+            try {
+                setLoading(true);
+                const response = await fetch('http://localhost:5206/players');
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                const data = await response.json();
+                setPlayers(data);
+            } catch (err) {
+                console.error('error fetching players:', err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchPlayers();
+    }, []);
+
     return(
         <>
+            <div>
+                
+            </div>
         </>
     )
 }
